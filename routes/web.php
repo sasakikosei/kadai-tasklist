8472b85fade4a017d23ba11a0e82489f7d541acb
sasakikosei <kkmondkk@outlook.jp>
2023-05-12 09:43:50 +0000

    top page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -27,5 +30,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('tasks/create', [TasksController::class, 'create'])->name('tasks.create');
+Route::get('tasks/store', [TasksController::class, 'store'])->name('tasks.store');
 
 require __DIR__.'/auth.php';
